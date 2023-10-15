4a71b2b60d2f1ea9c9aeabfedff3b1f51755f0a7
Daniel Haus <d@danielhaus.de>
2023-10-15 11:11:05 +0200

    test: add test

---
+++ src/lib/led-anim/generators.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from 'vitest';
+import { blink } from './generators';
+import type { LedAnimComponentConfig } from './types';
+
+describe('generators test', () => {
+  describe('blink test', () => {
+    test.each([
+      { x: 0, y: 0, t: 0, expected: 0.0 },
+      { x: 1, y: 0, t: 0, expected: 1.0 },
+      { x: 0, y: 0, t: 1, expected: 1.0 },
+      { x: 1, y: 0, t: 1, expected: 0.0 },
+    ])('blink($x, $y, t=$t) -> $expected', ({ x, y, t, expected }) => {
+      expect(blink(x, y, { t } as LedAnimComponentConfig)).toBe(expected);
+    });
+  });
+});
