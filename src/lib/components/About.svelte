5fb5d07dfb2f7071e8909963983411e967f4fb03
Daniel Haus <d@danielhaus.de>
2023-10-25 10:20:01 +0200

    feat. add heading

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,21 +1,25 @@
 <section>
 	<article>
-		<div class="column">
-			<p>
-				Ich bin <strong>IT-Berater</strong> und <strong>Softwareentwickler</strong> (Full-Stack,
-				Cloud, Web, Mobile, nativ) aus dem Rhein-Main-Gebiet. Als breitgefächerter Spezialist mit
-				Fokus auf <strong>benutzerfreundlicher</strong> und <strong>zuverlässiger</strong>,
-				durchdachter Software habe ich seit den späten 90er Jahren eine Vielzahl
-				überdurchschnittlich erfolgreicher Anwendungen realisiert.
-			</p>
-		</div>
+		<h4>Zur Person</h4>
 
-		<div class="column">
-			<p>
-				Meine Kunden umfassen bekannte Namen und Marken aus den Bereichen IT, Medien und TV,
-				Werbung, Banken und Finanzen, Versicherung, Pharma und Medizin, Unternehmensberatung,
-				High-End Audio, Automotive, Gesundheit, Bildung sowie aus dem öffentlichen Sektor.
-			</p>
+		<div class="columns-2">
+			<div class="column">
+				<p>
+					Ich bin <strong>IT-Berater</strong> und <strong>Softwareentwickler</strong> (Full-Stack,
+					Cloud, Web, Mobile, nativ) aus dem Rhein-Main-Gebiet. Als breitgefächerter Spezialist mit
+					Fokus auf <strong>benutzerfreundlicher</strong> und <strong>zuverlässiger</strong>,
+					durchdachter Software habe ich seit den späten 90er Jahren eine Vielzahl
+					überdurchschnittlich erfolgreicher Anwendungen realisiert.
+				</p>
+			</div>
+
+			<div class="column">
+				<p>
+					Meine Kunden umfassen bekannte Namen und Marken aus den Bereichen IT, Medien und TV,
+					Werbung, Banken und Finanzen, Versicherung, Pharma und Medizin, Unternehmensberatung,
+					High-End Audio, Automotive, Gesundheit, Bildung sowie aus dem öffentlichen Sektor.
+				</p>
+			</div>
 		</div>
 	</article>
 </section>
@@ -31,24 +35,32 @@
 	article {
 		@apply /**/
 			container
+			prose
+			dark:prose-invert
 			mx-auto
 			max-w-screen-lg
 			py-2
+			px-6
 			text-base
 			hyphens-auto
-			grid
-			gap-4
 			sm:text-xl
-			md:grid-cols-2
 			lg:max-w-5xl
 			/**/;
 	}
 
-	.column {
+	h4 {
 		@apply /**/
-			prose
-			dark:prose-invert
-			px-6
+			text-3xl
+			mt-5
+			mb-0
+			/**/;
+	}
+
+	.columns-2 {
+		@apply /**/
+			grid
+			gap-4
+			md:grid-cols-2
 			/**/;
 	}
 
