d2bd288c79d55a956b766a4d820ef8afeef42ba2
Daniel Haus <d@danielhaus.de>
2023-11-24 12:52:37 +0100

    feat: add content

--- src/lib/components/Services.svelte
+++ src/lib/components/Services.svelte
@@ -5,11 +5,12 @@
     <ul>
       <li><strong>Effiziente</strong> und <strong>effektive</strong> IT-Lösungen</li>
       <li><strong>Ganzheitliche</strong>, strategische Perspektive</li>
+      <li><strong>Kreativität</strong>, Stil und Erfahrung</li>
       <li>Schnelle, <strong>nachhaltige</strong> Softwareentwicklung</li>
       <li><strong>UI/UX</strong>, 2D/3D Grafikprogrammierung</li>
       <li>Audiovisuelle, <strong>interaktive</strong> Anwendungen</li>
       <li><strong>Cloud-native</strong>, Web, Mobile, Desktop, Server</li>
-      <li>CMS, APIs, Datenbanken, Python, PHP, HTML5, JS/TS, CSS, WebGL</li>
+      <li>CMS-Systeme, APIs, Datenbanken, Python, PHP, HTML5, JS/TS, CSS, WebGL</li>
       <li><strong>IT-Security</strong> und <strong>Datenschutz/DSGVO</strong> tiefenintegriert</li>
     </ul>
   </article>
