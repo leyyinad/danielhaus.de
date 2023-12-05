28b43ee7f0a370e058444e600a6218451ec64e4d
Daniel Haus <d@danielhaus.de>
2023-12-05 09:16:04 +0100

    feat: add code snippet section

---
+++ src/lib/components/widgets/CodeSnippet.svelte
@@ -0,0 +1,21 @@
+<section>
+  <figure></figure>
+</section>
+
+<style lang="postcss">
+  section {
+    @apply bg-black;
+  }
+
+  figure {
+    @apply relative
+      h-64
+      bg-[#1D131D]
+      bg-cover
+      bg-left
+      bg-no-repeat
+      lg:bg-auto;
+
+    background-image: url('code-persp.png');
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,11 @@
   import Hero from '$lib/components/Hero.svelte';
   import Projects from '$lib/components/Projects.svelte';
   import Services from '$lib/components/Services.svelte';
+  import CodeSnippet from '$lib/components/widgets/CodeSnippet.svelte';
 </script>
 
 <Hero />
 <About />
+<CodeSnippet />
 <Services />
 <Projects />
