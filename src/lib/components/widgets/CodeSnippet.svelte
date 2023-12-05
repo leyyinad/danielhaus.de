c4bd12458c12285f2f1a5428d61aea8b914f9a38
Daniel Haus <d@danielhaus.de>
2023-12-05 22:48:42 +0100

    feat: add code scroller

--- src/lib/components/widgets/CodeSnippet.svelte
+++ src/lib/components/widgets/CodeSnippet.svelte
@@ -1,21 +1,91 @@
+<script>
+  import cityGrid from '$lib/city/city-grid';
+  import { Engine } from '$lib/engine';
+  import EndlessScroller from './EndlessScroller.svelte';
+
+  const engineSrc = Engine.toString();
+  const grid = cityGrid
+    .toString()
+    .match(/.{1,30}/g)
+    ?.join('\n');
+
+  const scripts = [engineSrc, [grid, grid, grid].join('\n'), engineSrc, engineSrc, engineSrc];
+</script>
+
 <section>
-  <figure></figure>
+  {#each scripts as script, i}
+    <div class={`scroller scroller-${i}`}>
+      <EndlessScroller>
+        <pre>{script}</pre>
+      </EndlessScroller>
+    </div>
+  {/each}
 </section>
 
 <style lang="postcss">
   section {
-    @apply bg-black;
+    @apply relative
+      h-32
+      overflow-hidden
+      bg-black
+      bg-gradient-to-r
+      from-fungreen-900/20
+      to-fungreen-700/20;
   }
 
-  figure {
-    @apply relative
-      h-64
-      bg-[#1D131D]
-      bg-cover
-      bg-left
-      bg-no-repeat
-      lg:bg-auto;
+  .scroller {
+    @apply absolute
+      left-0
+      top-0
+      h-full
+      w-full
+      p-2;
 
-    background-image: url('$lib/images/code-persp.png');
+    --duration: 10s;
+    --delay: 0s;
+  }
+
+  .scroller-0 {
+    @apply w-1/2
+      font-bold;
+  }
+
+  .scroller-1 {
+    @apply left-3/4
+      scale-150
+      text-2xl
+      opacity-50;
+
+    --delay: -7s;
+    --duration: 8s;
+  }
+
+  .scroller-2 {
+    @apply left-1/3
+      scale-125
+      opacity-50;
+
+    --delay: -4s;
+    --duration: 5s;
+  }
+
+  .scroller-3 {
+    @apply left-2/3
+      -translate-x-12
+      opacity-75;
+
+    --delay: -9s;
+  }
+
+  .scroller-4 {
+    @apply left-3/4
+      opacity-40;
+
+    --delay: -12s;
+  }
+
+  pre {
+    @apply text-xs
+      text-fungreen-800;
   }
 </style>
---
+++ src/lib/components/widgets/EndlessScroller.svelte
@@ -0,0 +1,30 @@
+<div class="outer">
+  <div><slot /></div>
+</div>
+
+<style lang="postcss">
+  .outer {
+    @apply relative
+      h-full
+      overflow-hidden;
+  }
+
+  .outer > div {
+    @apply absolute
+      left-0
+      top-0
+      h-auto;
+
+    animation: scroll-anim var(--duration) linear var(--delay) infinite;
+  }
+
+  @keyframes scroll-anim {
+    0% {
+      @apply translate-y-full;
+    }
+
+    100% {
+      @apply -translate-y-full;
+    }
+  }
+</style>
