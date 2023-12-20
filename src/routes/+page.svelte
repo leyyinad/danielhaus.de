2d2acd0b8740a4c49d6b6bbf7a5922468b549bd7
Daniel Haus <d@danielhaus.de>
2023-12-20 12:00:23 +0100

    feat: add og tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,6 +13,25 @@
     name="description"
     content="Mein Name ist Daniel Haus. Ich bin IT-Berater und Softwareentwickler aus dem Rhein-Main-Gebiet."
   />
+
+  <meta property="og:url" content="https://danielhaus.de" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Daniel Haus" />
+  <meta
+    property="og:description"
+    content="IT-Berater und Softwareentwickler im Rhein-Main-Gebiet."
+  />
+  <meta property="og:image" content="https://danielhaus.de/poster.png" />
+
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta property="twitter:domain" content="danielhaus.de" />
+  <meta property="twitter:url" content="https://danielhaus.de" />
+  <meta name="twitter:title" content="Daniel Haus" />
+  <meta
+    name="twitter:description"
+    content="IT-Berater und Softwareentwickler im Rhein-Main-Gebiet."
+  />
+  <meta name="twitter:image" content="https://danielhaus.de/poster.png" />
 </svelte:head>
 
 <Hero />
