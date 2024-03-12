0e5701c67b3a58a90a6ea29cf66d22db15ec65de
Daniel Haus <d@danielhaus.de>
2024-03-12 13:34:33 +0100

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import { glslify } from 'vite-plugin-glslify';
+import glsl from 'vite-plugin-glsl';
 import { skramble } from './skramble';
 
 export default defineConfig(({ mode }) => ({
-  plugins: [sveltekit(), glslify(), skramble({ mode, vars: ['CONTACT_EMAIL', 'CONTACT_PHONE'] })],
+  plugins: [sveltekit(), glsl(), skramble({ mode, vars: ['CONTACT_EMAIL', 'CONTACT_PHONE'] })],
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   }
