b81b83f3ad196ea77941e98abd9c598af7fbc838
Daniel Haus <d@danielhaus.de>
2025-01-17 09:52:00 +0100

    chore: prepare config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+//import browserslist from 'browserslist';
+//import { browserslistToTargets } from 'lightningcss';
+import { enhancedImages } from '@sveltejs/enhanced-img';
+import type { UserConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
+import type { UserConfig as VitestUserConfig } from 'vitest/node';
 
 export default {
-  plugins: [sveltekit(), glsl()],
+  plugins: [enhancedImages(), sveltekit(), glsl()],
+
+  //css: {
+  //  transformer: 'lightningcss',
+  //  lightningcss: {
+  //    targets: browserslistToTargets(browserslist('>= 0.25% and not dead'))
+  //  }
+  //},
+  //
+  //build: {
+  //  cssMinify: 'lightningcss'
+  //},
+
   test: {
     include: ['src/**/*.{test,spec}.{js,ts}']
   }
-};
+} satisfies UserConfig | { test: VitestUserConfig };
