8a9605d4e6379fd2daea4cac35874eee729d94f8
Daniel Haus <d@danielhaus.de>
2025-01-17 10:23:30 +0100

    refactor: eliminate tailwindcss

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -26,11 +26,33 @@
 
 <style lang="postcss">
   section {
-    @apply bg-black
-			py-8;
+    background-color: black;
+    padding: 2rem 0;
+
+    /* @apply bg-black py-8; */
   }
 
   article {
+    margin: 0 auto;
+    padding: 0.5rem 1.5rem;
+    width: var(--container-width);
+    max-width: 1024px;
+    color: white;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    hyphens: auto;
+
+    @media (min-width: 640px) {
+      font-size: 1.25rem;
+      line-height: 1.75rem;
+    }
+
+    @media (min-width: 1024px) {
+      max-width: 64rem;
+    }
+
+    /*
     @apply container
 			prose
 			prose-invert
@@ -42,24 +64,63 @@
 			text-base
 			sm:text-xl
 			lg:max-w-5xl;
+      */
   }
 
   h4 {
+    margin-top: 1.25rem;
+    margin-bottom: 0;
+    font-weight: 900;
+    font-size: 1.875rem /* 30px */;
+    line-height: 2.25rem /* 36px */;
+
+    @media (min-width: 768px) {
+      font-size: 2.25rem /* 36px */;
+      line-height: 2.5rem /* 40px */;
+    }
+
+    /*
     @apply mb-0
 			mt-5
 			text-3xl
 			font-black
       md:text-4xl;
+      */
+  }
+
+  p {
+    margin: 1.25rem 0;
+
+    strong {
+      font-weight: 600;
+    }
   }
 
   .columns-2 {
+    display: grid;
+    gap: 1rem;
+
+    @media (min-width: 768px) {
+      grid-template-columns: repeat(2, minmax(0, 1fr));
+    }
+
+    /*
     @apply grid
 			gap-4
 			md:grid-cols-2;
+      */
   }
 
   .column:not(:first-child) {
+    margin-top: -2rem;
+
+    @media (min-width: 768px) {
+      margin-top: 0;
+    }
+
+    /*
     @apply -mt-8
 			md:mt-0;
+    */
   }
 </style>
