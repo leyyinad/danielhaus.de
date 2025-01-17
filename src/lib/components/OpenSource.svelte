6fdf7ce9b0e4d784c32aa950d89912444736106f
Daniel Haus <d@danielhaus.de>
2025-01-17 11:53:28 +0100

    refactor: eliminate tailwindcss

--- src/lib/components/OpenSource.svelte
+++ src/lib/components/OpenSource.svelte
@@ -20,13 +20,42 @@
 
 <style lang="postcss">
   section {
+    border-top: 2px solid black;
+    border-bottom: 2px solid black;
+    background-color: var(--violetshirt-950);
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+
+    /*
     @apply border-y-2
       border-y-black
       bg-violetshirt-950
       py-8;
+      */
   }
 
   article {
+    margin-right: auto;
+    margin-left: auto;
+    padding-right: 1.5rem;
+    padding-left: 1.5rem;
+    width: var(--container-width);
+    max-width: 1024px;
+    color: rgb(from var(--violetshirt-300) r g b / 80%);
+    font-size: 1rem;
+    line-height: 1.5rem;
+    hyphens: auto;
+
+    @media (min-width: 640px) {
+      font-size: 1.25rem /* 20px */;
+      line-height: 1.75rem /* 28px */;
+    }
+
+    @media (min-width: 1024px) {
+      max-width: 64rem /* 1024px */;
+    }
+
+    /*
     @apply container
       prose
       prose-invert
@@ -38,21 +67,49 @@
       text-violetshirt-300/80
 			sm:text-xl
 			lg:max-w-5xl;
+      */
   }
 
   article a {
+    color: rgb(from var(--violetshirt-300) r g b / 80%);
+    font-weight: 300;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+      text-underline-offset: 4px;
+    }
+
+    /*
     @apply font-light
       text-violetshirt-300/80
       no-underline
       hover:underline
-      hover:underline-offset-4;
+      hover:underline-offset-4;*/
   }
 
   article a strong {
-    @apply font-medium text-white;
+    color: white;
+    font-weight: 500;
+
+    /* @apply font-medium text-white;*/
   }
 
   h4 {
+    filter: drop-shadow(0 1px 2px rgb(0 0 0 / 0.1)) drop-shadow(0 1px 1px rgb(0 0 0 / 0.06));
+    margin-top: 1.25rem;
+    margin-bottom: 1rem;
+    color: var(--violetshirt-300);
+    font-weight: 900;
+    font-size: 1.875rem;
+    line-height: 2.25rem;
+
+    @media (min-width: 768px) {
+      font-size: 2.25rem;
+      line-height: 2.5rem;
+    }
+
+    /*
     @apply mb-4
 			mt-5
 			text-3xl
@@ -60,5 +117,14 @@
       text-violetshirt-300
       drop-shadow
       md:text-4xl;
+      */
+  }
+
+  p {
+    margin: 1.5rem 0;
+
+    strong {
+      font-weight: 600;
+    }
   }
 </style>
